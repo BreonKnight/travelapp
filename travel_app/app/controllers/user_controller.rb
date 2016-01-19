38a42666af7700ef18df2d28dc5bbd3cb7cebcbe
revalidate <reva.bhatt@gmail.com>
2016-01-19 12:17:41 -0800

    added new and create method to User Controller

--- travel_app/app/controllers/user_controller.rb
+++ travel_app/app/controllers/user_controller.rb
@@ -1,2 +1,16 @@
 class UserController < ApplicationController
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    user_params = params.require(:user).permit(:first_name, :last_name, :email, :password, :current_city)
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to user_path(@user)
+  end
+
 end
