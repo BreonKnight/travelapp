0a5cb0ce08c7a74b1bb72d15108789ef5b7db0f7
Breon Knight <Breonknight7@gmail.com>
2016-01-20 14:16:04 -0800

    schema added

--- travel_app/db/schema.rb
+++ travel_app/db/schema.rb
@@ -11,10 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160119230719) do
+ActiveRecord::Schema.define(version: 20160120201812) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cities", force: :cascade do |t|
+    t.string   "name"
+    t.string   "country"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "entries", force: :cascade do |t|
     t.string   "title"
