bcc84b7ca44a05bfcfa238195bad9f0f34cc1616
mseegan <mseegan@gmail.com>
2016-01-20 13:57:40 -0800

    added profile page that only shows the logged in user's profile

--- travel_app/app/controllers/users_controller.rb
+++ travel_app/app/controllers/users_controller.rb
@@ -24,6 +24,11 @@ class UsersController < ApplicationController
   def show
   end
 
+  # def profile
+  #   user_id = current_user.id
+  #   @user = User.find_by_id(user_id)
+  # end
+
   def edit
   end
 
@@ -41,7 +46,7 @@ class UsersController < ApplicationController
   private
 
   def set_user
-    user_id = params[:id]
+    user_id = params[:id] || current_user.id
     @user = User.find_by_id(user_id)
   end
 
