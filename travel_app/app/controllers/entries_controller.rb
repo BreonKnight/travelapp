89217be6573989ad8cf0a35dd11c7a59d47e0b4a
Breon Knight <Breonknight7@gmail.com>
2016-01-19 16:28:03 -0800

    added entries controller

--- travel_app/app/controllers/entries_controller.rb
+++ travel_app/app/controllers/entries_controller.rb
@@ -1,43 +1,44 @@
 class EntriesController < ApplicationController
-	def index
-		@entries = Entry.all
-		render :index
-	end
+  def index
+    @entries = Entry.all
+    render :index
+  end
 
-	def new
-		@entries = Entry.new
-	end
+  def new
+    @entries = Entry.new
+  end
 
-	def create
-		@entry = Entry.new(entry_params)
-		if @entry.save
-			redirect_to '/'
-		else
-		flash[:error] = "Successfully failed bit..."
-		redirect_to new_entry_path
-		end
-	end
+  def create
+    @entry = Entry.new(entry_params)
+    if @entry.save
+      redirect_to '/'
+    else
+    flash[:error] = "Successfully failed bit..."
+    redirect_to new_entry_path
+    end
+  end
 
-	def show
-		@entry = Entry.find(params[:id])
-		render :show
-	end
+  def show
+    @entry = Entry.find(params[:id])
+    render :show
+  end
 
-	def edit
-		entry_id = params[:id]
-		@entry = Entry.find_by_id(entry_id)
-		render :edit
+  def edit
+    entry_id = params[:id]
+    @entry = Entry.find_by_id(entry_id)
+    render :edit  
+  end
 
-	end
+  def update
 
-	def update
+  end
 
-	end
+  def destroy
 
-	def destroy
+  end
 
-	private
-	def entry_params
-		params.require(:entry).permit(:title, :body)
-	end
+  private
+  def entry_params
+    params.require(:entry).permit(:title, :body)
+  end
 end
