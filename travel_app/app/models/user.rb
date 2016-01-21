2185094208b274da6dd111f4e6e74c83f990ee8b
Daniel Lwo <danielxji6@gmail.com>
2016-01-20 17:50:58 -0800

    add two city referance in user table

--- travel_app/app/models/user.rb
+++ travel_app/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
   has_many :entries
+  has_many :primary_tickets, :class_name => "City", :foreign_key => "current_user"
+  has_many :secondary_tickets, :class_name => "City", :foreign_key => "home_city"
+
   has_secure_password
 end
