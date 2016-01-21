3a157827c53ec10cd567474cfd6b99c3040f4ad6
Daniel Lwo <danielxji6@gmail.com>
2016-01-21 00:06:52 -0800

    add city_id column to entries table

--- travel_app/app/models/user.rb
+++ travel_app/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_many :entries
-  has_many :primary_tickets, :class_name => "City", :foreign_key => "current_user"
-  has_many :secondary_tickets, :class_name => "City", :foreign_key => "home_city"
+  belongs_to :current_city, :class_name => "City", :foreign_key => "current_city_id"
+  belongs_to :home_city, :class_name => "City", :foreign_key => "home_city_id"
 
   has_secure_password
 end
